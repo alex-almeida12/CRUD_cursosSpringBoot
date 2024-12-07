6f9de84c86530a1be244ee6d92fae09bddbf20e9
Alex Almeida <alex.almeida12@gmail.com>
2024-12-07 20:26:55 -0300

    ajustando dependencias

--- src/test/java/br/com/alexalmeida/crudcursos/CrudcursosApplicationTests.java
+++ src/test/java/br/com/alexalmeida/crudcursos/CrudcursosApplicationTests.java
@@ -3,6 +3,7 @@ package br.com.alexalmeida.crudcursos;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class CrudcursosApplicationTests {
 
